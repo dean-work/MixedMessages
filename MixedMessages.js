221547ac925fb8a42b608a9c1aa39cfe17f3c5ec
Dean <dean.aus.work@gmail.com>
2020-12-31 18:37:21 +1100

    Added Seasons

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,1 +1,3 @@
 const starSigns = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+const seasons = {"Summer": ["December", "January", "February"], "Autumn": ["March", "April", "May"], 
+                "Winter": ["June", "July", "August"], "Spring" : ["September", "October", "November"]};
