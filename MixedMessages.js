cc8519494e5281f5c5a5fb8a76a1084ca9280e15
Dean <dean.aus.work@gmail.com>
2020-12-31 18:38:35 +1100

    Added Numbers

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,3 +1,4 @@
 const starSigns = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
 const seasons = {"Summer": ["December", "January", "February"], "Autumn": ["March", "April", "May"], 
                 "Winter": ["June", "July", "August"], "Spring" : ["September", "October", "November"]};
+const numbers = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
